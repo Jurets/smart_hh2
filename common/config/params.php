ab9b2abae0e3a20240f04dde958977868b297d3c
Jurets <jurets75@gmail.com>
2014-11-04 17:10:44 +0200

    http://mantis.cmapt.com.ua/view.php?id=1520

--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,13 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+    //system languages
+    'languages' => array(
+            'en' => 'English',
+            'ru' => 'Russian',
+            'pt' => 'Portuguese',
+            'es' => 'Spanish'
+    ),
+    //key for google translater
+    'apiLanguages' => 'AIzaSyAu846bCVJtTMDQBeT11v-pmSr-p_htx8g'
 ];
