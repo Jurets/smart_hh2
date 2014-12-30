498c4e44e1853b27164222d359313c6990c48bda
andrew <volpacchiotto@ukr.net>
2014-12-30 18:19:22 +0200

    исправление отсутствия выхода на рисунок

--- frontend/views/ticket/_tisket.php
+++ frontend/views/ticket/_tisket.php
@@ -13,8 +13,8 @@ use yii\helpers\Url;
     </div>
     <div class="clearfix"></div>
     <div class="autor left">
-        <?php //var_dump($model->user->profile->files->code);die; ?>
-        <img class="left" style="width:45px;" src="<?php echo Yii::$app->params['upload.url'].'/'.$model->user->profile->files->code?>" alt="avatar">
+        <?php $photo = $model->user->profile->files; ?>
+        <img class="left" style="width:45px;" src="<?php echo (!is_null($photo)) ? Yii::$app->params['upload.url'].'/'.$model->user->profile->files->code : ''?>" alt="avatar">
         <p>Alex B.<img src="/images/star5.png"/><span class="vote">(3.5 based on 40 votes)</span></p>
         <p>Active 35 jobs</p>
     </div>
