c0567111f75c385751f8cc73871b1a71271ee136
andrew <volpacchiotto@ukr.net>
2015-01-12 11:59:04 +0200

    подготовка базового offer-контроллера

---
+++ frontend/controllers/OfferController.php
@@ -0,0 +1,10 @@
+<?php
+namespace frontend\controllers;
+use Yii;
+use yii\data\ActiveDataProvider;
+use common\components\Controller;
+use yii\filters\VerbFilter;
+use yii\helpers\Url;
+use common\models\Offer;
+use common\models\OfferHistory;
+
