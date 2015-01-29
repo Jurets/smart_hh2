3e9018c27e4c8c50bb0e6ece3028d4e78135dc49
andrew <volpacchiotto@ukr.net>
2015-01-29 19:52:43 +0200

    Вечерний патч

--- common/modules/user/views/default/_profile_spec.php
+++ common/modules/user/views/default/_profile_spec.php
@@ -5,25 +5,17 @@ use yii\helpers\Url;
 <div class="widget specialties">
             <h1>My <span class="red"><?= count($userSpecialities) ?></span> Specialties</h1>
             <div class="specialty-holder">
-<!--                <div class="category-item">
-                    <a href="#" class="specialty-item">
-                        <div class="icon-items">
-                            <img src="/images/categories/moving.png" alt="Moving"/>                                            
-                        </div>
-                        <p>Moving</p>                     
-                    </a>
-
-                </div>-->
             <?php if (!is_null($userSpecialities)) { ?>
                 <?php foreach ($userSpecialities as $spec) { ?>
                     <div class="category-item">
-                        <a href="#" class="speciality-item">
+                        <a href="#" class="specialty-item">
                             <div class="icon-items">
-                                <img src="<?= Yii::$app->params['images.url'] . '/' . $spec->picture ?>" alt="">
+                                <img src="<?= Yii::$app->params['images.url'] . '/categories/' . $spec->picture ?>" alt="<?=$spec->name?>">
                             </div>
+                            <p><?=$spec->name?></p>
                         </a>
                     </div>
                 <?php } ?>
             <?php } ?>
             </div> 
-        </div>
+</div>
