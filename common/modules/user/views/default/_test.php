6776e29947f14da8f290aa3875fc91469014fe8d
andrew <volpacchiotto@ukr.net>
2015-02-05 22:50:46 +0200

    какая-то мелочь

--- common/modules/user/views/default/_test.php
+++ common/modules/user/views/default/_test.php
@@ -4,14 +4,13 @@ use yii\helpers\Url;
 use yii\helpers\Html;
 ?>
 <?php 
+var_dump($users);
 echo Html::beginForm(Url::to(['test']), 'post');
 echo Html::label('From User').'&nbsp;';
 echo Html::dropDownList('from_user',NULL, $users).'<br><br>';
 echo Html::label('From User').'&nbsp;';
 echo Html::dropDownList('to_user',NULL, $users).'<br><br>';
-echo Html::textarea('message', NULL, ['rows'=>10, 'cols'=>100]);
+echo Html::textarea('message', NULL, ['rows'=>10, 'cols'=>100]).'<br><br>';
 echo Html::submitButton();
 echo Html::endForm();
-
-
 ?>
