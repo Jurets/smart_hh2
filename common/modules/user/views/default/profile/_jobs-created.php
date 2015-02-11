cb1b24df9d5d7e4548f052fd19157879f9ed2fa0
vlysychenko <vlysychenko@gmail.com>
2015-02-11 16:41:14 +0200

    Перенес "your jobs" в частичное представление

---
+++ common/modules/user/views/default/profile/_jobs-created.php
@@ -0,0 +1,176 @@
+<div class="reviews-holder">
+    <div class="task-item info-border">
+        <div class="task-info-price">
+            <p class="price">&dollar;500</p>
+            <p class="measurement">week</p>
+        </div>
+        <div class="task-info-meta">
+            <a  href="#" class="title">Web site development for a law company in Moscow</a>
+            <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+        </div>
+        <div class="clearfix"></div>
+        <div class="date-time right">
+            5:15 JAN 01, 2015 <br/>      
+            Moscow, RU
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <div class="task-item info-border">
+        <div class="task-info-price">
+            <p class="price">&dollar;500</p>
+            <p class="measurement">week</p>
+        </div>
+        <div class="task-info-meta">
+            <a  href="#" class="title">Web site development for a law company in Moscow</a>
+            <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+        </div>
+        <div class="clearfix"></div>
+        <div class="date-time right">
+            5:15 JAN 01, 2015 <br/>      
+            Moscow, RU
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <div class="task-item info-border">
+        <div class="task-info-price">
+            <p class="price">&dollar;500</p>
+            <p class="measurement">week</p>
+        </div>
+        <div class="task-info-meta">
+            <a  href="#" class="title">Web site development for a law company in Moscow</a>
+            <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+        </div>
+        <div class="clearfix"></div>
+        <div class="date-time right">
+            5:15 JAN 01, 2015 <br/>      
+            Moscow, RU
+        </div>
+        <div class="clearfix"></div>
+    </div>            
+    <a class="btn btn-width">SHOW MORE</a>
+    <div class="collapse">
+        <div class="task-item info-border">
+            <div class="task-info-price">
+                <p class="price">&dollar;500</p>
+                <p class="measurement">week</p>
+            </div>
+            <div class="task-info-meta">
+                <a  href="#" class="title">Web site development for a law company in Moscow</a>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+            </div>
+            <div class="clearfix"></div>
+            <div class="date-time right">
+                5:15 JAN 01, 2015 <br/>      
+                Moscow, RU
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <div class="task-item info-border">
+            <div class="task-info-price">
+                <p class="price">&dollar;500</p>
+                <p class="measurement">week</p>
+            </div>
+            <div class="task-info-meta">
+                <a  href="#" class="title">Web site development for a law company in Moscow</a>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+            </div>
+            <div class="clearfix"></div>
+            <div class="date-time right">
+                5:15 JAN 01, 2015 <br/>      
+                Moscow, RU
+            </div>
+            <div class="clearfix"></div>
+        </div>   
+        <div class="task-item info-border">
+            <div class="task-info-price">
+                <p class="price">&dollar;500</p>
+                <p class="measurement">week</p>
+            </div>
+            <div class="task-info-meta">
+                <a  href="#" class="title">Web site development for a law company in Moscow</a>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+            </div>
+            <div class="clearfix"></div>
+            <div class="date-time right">
+                5:15 JAN 01, 2015 <br/>      
+                Moscow, RU
+            </div>
+            <div class="clearfix"></div>
+        </div>   
+        <div class="task-item info-border">
+            <div class="task-info-price">
+                <p class="price">&dollar;500</p>
+                <p class="measurement">week</p>
+            </div>
+            <div class="task-info-meta">
+                <a  href="#" class="title">Web site development for a law company in Moscow</a>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+            </div>
+            <div class="clearfix"></div>
+            <div class="date-time right">
+                5:15 JAN 01, 2015 <br/>      
+                Moscow, RU
+            </div>
+            <div class="clearfix"></div>
+        </div>   
+        <div class="task-item info-border">
+            <div class="task-info-price">
+                <p class="price">&dollar;500</p>
+                <p class="measurement">week</p>
+            </div>
+            <div class="task-info-meta">
+                <a  href="#" class="title">Web site development for a law company in Moscow</a>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+            </div>
+            <div class="clearfix"></div>
+            <div class="date-time right">
+                5:15 JAN 01, 2015 <br/>      
+                Moscow, RU
+            </div>
+            <div class="clearfix"></div>
+        </div>   
+        <div class="task-item info-border">
+            <div class="task-info-price">
+                <p class="price">&dollar;500</p>
+                <p class="measurement">week</p>
+            </div>
+            <div class="task-info-meta">
+                <a  href="#" class="title">Web site development for a law company in Moscow</a>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+            </div>
+            <div class="clearfix"></div>
+            <div class="date-time right">
+                5:15 JAN 01, 2015 <br/>      
+                Moscow, RU
+            </div>
+            <div class="clearfix"></div>
+        </div>   
+        <div class="task-item info-border">
+            <div class="task-info-price">
+                <p class="price">&dollar;500</p>
+                <p class="measurement">week</p>
+            </div>
+            <div class="task-info-meta">
+                <a  href="#" class="title">Web site development for a law company in Moscow</a>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+            </div>
+            <div class="clearfix"></div>
+            <div class="date-time right">
+                5:15 JAN 01, 2015 <br/>      
+                Moscow, RU
+            </div>
+            <div class="clearfix"></div>
+        </div>                   
+    </div>
+</div>    
+
