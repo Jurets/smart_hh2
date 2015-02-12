4c84ce74c9784c59562e601336aa23e578804cb3
vlysychenko <vlysychenko@gmail.com>
2015-02-12 13:54:49 +0200

    Добавил выпадающий список тикетов с непрочитанными комментариями

--- frontend/views/layouts/parts/header_login.php
+++ frontend/views/layouts/parts/header_login.php
@@ -1,7 +1,9 @@
 <?php
 use yii\helpers\Url;
+use yii\helpers\Html;
 
 $currentUser = Yii::$app->user->getIdentity();
+$allNewTicketsCommentsCount = ($currentUser !== null) ? $currentUser->getNewTicketCommentsCount() : 0;
 ?>
 <!-- header login -->
 
@@ -11,10 +13,20 @@ $currentUser = Yii::$app->user->getIdentity();
     </div>
     <div class="top-nav col-xs-12 col-sm-12 col-md-12 col-lg-9">
         <div class="status">
-            <a href="#" class=""><img src="/images/icon-letter.png" alt="letter"/>
+            <span>
+            <a href="#" class="" <?= $allNewTicketsCommentsCount ? 'data-toggle="dropdown"' : '' ?>><img src="/images/icon-letter.png" alt="letter"/>
             <?= Yii::t('app', '{n, plural, =1{<span>#</span> new message} other{<span>#</span> new messages}}', [
-                'n' => ($currentUser !== null) ? $currentUser->getNewTicketCommentsCount() : '0'
+                'n' => $allNewTicketsCommentsCount
                 ]); ?>
+            </a>
+            <?php if($allNewTicketsCommentsCount): ?>
+                <ul class="dropdown-menu" role="menu">
+                    <?php foreach($currentUser->getTicketsWithNewComments() as $ticket): ?>
+                    <li><a href="<?= Url::to(['/ticket/view', 'id' => $ticket['id']])?>"><?= Html::encode($ticket['title'])?> <span class="red">+<?= Html::encode($ticket['comments_count']) ?></span></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+            </span>
             <a href="#" class=""><img src="/images/icon-bell.png" alt="bell"/><span>115</span>&nbsp;<?=Yii::t('app', 'new offers')?></a>           
             <?php if(Yii::$app->controller->action->id === 'profile'){ ?>
             <a href="<?=Url::to(['/user/cabinet'],true)?>" class=""><img src="/images/icon-pen.png" alt="pen"><?=Yii::t('app','Edit Profile')?></a>
