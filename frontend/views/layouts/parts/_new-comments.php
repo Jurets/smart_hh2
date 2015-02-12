98d0e005dcc766250511999ee8f577ded5793664
vlysychenko <vlysychenko@gmail.com>
2015-02-12 15:10:45 +0200

    Добавил кнопку для очистки списка непрочитанных комментариев

---
+++ frontend/views/layouts/parts/_new-comments.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\helpers\Url;
+use yii\helpers\Html;
+
+$currentUser = Yii::$app->user->getIdentity();
+$allNewTicketsCommentsCount = ($currentUser !== null) ? $currentUser->getNewTicketCommentsCount() : 0;
+?>
+<a href="#" class="" <?= $allNewTicketsCommentsCount ? 'data-toggle="dropdown"' : '' ?>><img src="/images/icon-letter.png" alt="letter"/>
+    <?=
+    Yii::t('app', '{n, plural, =1{<span>#</span> new message} other{<span>#</span> new messages}}', [
+        'n' => $allNewTicketsCommentsCount
+    ]);
+    ?>
+</a>
+    <?php if ($allNewTicketsCommentsCount): ?>
+    <ul class="dropdown-menu" role="menu">
+    <?php foreach ($currentUser->getTicketsWithNewComments() as $ticket): ?>
+            <li><a href="<?= Url::to(['/ticket/view', 'id' => $ticket['id']]) ?>"><?= Html::encode($ticket['title']) ?> <span class="red">+<?= Html::encode($ticket['comments_count']) ?></span></a></li>
+    <?php endforeach; ?>
+        <li><span class="btn btn-width btn-average" id="clear-comments-btn" data-url="<?= Url::to(['/site/clear-new-comments']) ?>">Clear</span></li>
+    </ul>
+<?php endif; ?>
+
--- frontend/views/layouts/parts/header_login.php
+++ frontend/views/layouts/parts/header_login.php
@@ -1,9 +1,6 @@
 <?php
 use yii\helpers\Url;
 use yii\helpers\Html;
-
-$currentUser = Yii::$app->user->getIdentity();
-$allNewTicketsCommentsCount = ($currentUser !== null) ? $currentUser->getNewTicketCommentsCount() : 0;
 ?>
 <!-- header login -->
 
@@ -13,19 +10,8 @@ $allNewTicketsCommentsCount = ($currentUser !== null) ? $currentUser->getNewTick
     </div>
     <div class="top-nav col-xs-12 col-sm-12 col-md-12 col-lg-9">
         <div class="status">
-            <span>
-            <a href="#" class="" <?= $allNewTicketsCommentsCount ? 'data-toggle="dropdown"' : '' ?>><img src="/images/icon-letter.png" alt="letter"/>
-            <?= Yii::t('app', '{n, plural, =1{<span>#</span> new message} other{<span>#</span> new messages}}', [
-                'n' => $allNewTicketsCommentsCount
-                ]); ?>
-            </a>
-            <?php if($allNewTicketsCommentsCount): ?>
-                <ul class="dropdown-menu" role="menu">
-                    <?php foreach($currentUser->getTicketsWithNewComments() as $ticket): ?>
-                    <li><a href="<?= Url::to(['/ticket/view', 'id' => $ticket['id']])?>"><?= Html::encode($ticket['title'])?> <span class="red">+<?= Html::encode($ticket['comments_count']) ?></span></a></li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
+            <span id="new-comments-container">
+                <?= $this->render('_new-comments') ?>
             </span>
             <a href="#" class=""><img src="/images/icon-bell.png" alt="bell"/><span>115</span>&nbsp;<?=Yii::t('app', 'new offers')?></a>           
             <?php if(Yii::$app->controller->action->id === 'profile'){ ?>
