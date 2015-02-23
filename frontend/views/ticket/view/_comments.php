c010adb3172f5d0f7fa554535a506b369fbb9b39
vlysychenko <vlysychenko@gmail.com>
2015-02-23 09:49:58 +0200

    fix 10) Когда отвечаешь на коммент получается какая-то визуальная глупость. и окно для реплая, и окно для своего коммента. Можно как-то почистить?
    Удалил возможность создания новых комментов на странице ticket/view

--- frontend/views/ticket/view/_comments.php
+++ frontend/views/ticket/view/_comments.php
@@ -28,16 +28,4 @@ $newComment = new \common\models\TicketComments();
             </div>
         <?php endif; ?>
     <?php endforeach; ?>
-    <p class="title">Write a Comment:</p>
-    <?php
-    $form = yii\widgets\ActiveForm::begin([
-                'action' => ['ticket/add-comment']
-            ])
-    ?>
-    <?= $form->field($newComment, 'text')->textarea()->label(false) ?>
-    <?= $form->field($newComment, 'ticket_id')->hiddenInput(['value' => $model->id])->label(false) ?>
-    <?= $form->field($newComment, 'user_id')->hiddenInput(['value' => Yii::$app->user->id])->label(false) ?>
-    <?= yii\helpers\Html::hiddenInput('redirect', 'ticket/view') ?>
-    <?= yii\helpers\Html::submitButton(Yii::t('app', 'Send'), ['id' => 'comment-send', 'class' => 'btn btn-success']) ?>
-    <?php yii\widgets\ActiveForm::end(); ?>
 </div>
