8d8b98df8061e1e3d2d186e9be120fdfbdfc631a
vlysychenko <vlysychenko@gmail.com>
2015-02-23 11:40:21 +0200

    fix 14) Нажимание на "не забудьте о приближающейся работе" сейчас ведет в страницу 403 и ошибку. А должно бы на работу вести.

--- frontend/views/layouts/parts/bell/bell_fd_up.php
+++ frontend/views/layouts/parts/bell/bell_fd_up.php
@@ -4,7 +4,7 @@ use yii\helpers\Url;
 use yii\helpers\Html;
 ?>
 <li>
-    <a href="<?= Url::to(['/ticket/view', 'id' => $ticket['id']]) ?>">
+    <a href="<?= Url::to(['/ticket/review', 'id' => $ticket['id']]) ?>">
         <?= Yii::t('app','Don\'t miss a job You apllied to: {title} is on {date}',[
             'title' => Html::encode($ticket['title']),
             'date' => Html::encode($ticket['date']),
