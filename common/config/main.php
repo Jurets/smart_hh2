f01deed54140b2f37f5a6e47f1b1b3b523d891fe
vlysychenko <vlysychenko@gmail.com>
2015-02-27 09:59:53 +0200

    Добавил компонент paypal в конфиг

--- common/config/main.php
+++ common/config/main.php
@@ -24,7 +24,22 @@ return [
         ],
         'notification' => [
             'class' => 'common\components\Notification',
-        ]
+        ],
+        'paypal' => [
+            'class' => 'marciocamello\Paypal',
+            'clientId' => 'you_client_id',
+            'clientSecret' => 'you_client_secret',
+            'isProduction' => false,
+            // This is config file for the PayPal system
+            'config' => [
+                'http.ConnectionTimeOut' => 30,
+                'http.Retry' => 1,
+                'mode' => \marciocamello\Paypal::MODE_SANDBOX, // development (sandbox) or production (live) mode
+                'log.LogEnabled' => YII_DEBUG ? 1 : 0,
+                'log.FileName' => '@runtime/logs/paypal.log',
+                'log.LogLevel' => \marciocamello\Paypal::LOG_LEVEL_FINE,
+            ]
+        ],
     ],
     'modules' => [
         'user' => [
