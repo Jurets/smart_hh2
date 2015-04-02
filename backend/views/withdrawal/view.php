c06f5b5d9800ac2bdb9a17def0152e492dbff838
andrew <volpacchiotto@ukr.net>
2015-04-02 21:25:08 +0300

    вернул кнопку удалить в view withdrawal

--- backend/views/withdrawal/view.php
+++ backend/views/withdrawal/view.php
@@ -18,13 +18,13 @@ $this->params['breadcrumbs'][] = $this->title;
         <?php //echo Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?php echo Html::a(Yii::t('app', 'Return'), ['index'], ['class' => 'btn btn-primary'])?>
         <?php 
-//        echo Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-//            'class' => 'btn btn-danger',
-//            'data' => [
-//                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-//                'method' => 'post',
-//            ],
-//        ]) 
+        echo Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                'method' => 'post',
+            ],
+        ]) 
         ?>
     </p>
 
