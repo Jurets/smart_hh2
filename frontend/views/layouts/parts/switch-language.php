741fa968ef1dd5688655f11c201f65e3341b3ae3
andrew <pernalsky@gmail.com>
2015-04-18 22:50:04 +0300

    добавил еще два языка. Пока без привязки к бд.

--- frontend/views/layouts/parts/switch-language.php
+++ frontend/views/layouts/parts/switch-language.php
@@ -11,8 +11,10 @@ $language = Commonhelper::getLanguage();
 ?>
 <?php // temporary hardcode languages ISO 639-1 ?>
 <select id="language">
-    <option value="en" data-imagesrc="/images/language-icon.png" <?=$language == 'en' ? ' selected=""' : ''?>>English</option>
-    <option value="es" data-imagesrc="/images/language-icon.png" <?=$language == 'es' ? ' selected=""' : ''?>>Español</option>
+    <option value="en" data-imagesrc="/images/lang/en-small.png" <?=$language == 'en' ? ' selected=""' : ''?>>English</option>
+    <option value="ru" data-imagesrc="/images//lang/ru-small.png" <?=$language == 'ru' ? ' selected=""' : ''?>>Русский</option>
+    <option value="spa" data-imagesrc="/images/lang/spa-small.png" <?=$language == 'spa' ? ' selected=""' : ''?>>Español</option>
+    <option value="por" data-imagesrc="/images/lang/por-small.png" <?=$language == 'por' ? ' selected=""' : ''?>>português</option>
 </select>
 
 <div data-langUrlChange="<?=Url::to(['site/languageswitcher'],true)?>"></div>
