062c0290a384754f86886ba8f05c63905d612990
dgribanov <denisgribanov@yandex.ru>
2015-04-29 17:29:06 +0300

    исправление бага при сохранении языков

--- frontend/views/registration/_languages_form.php
+++ frontend/views/registration/_languages_form.php
@@ -5,13 +5,13 @@ use frontend\helpers\ContactsHelper;
 
 <?php
     $nativeLangList = !empty($userLanguage->language_id) ? $languages : array_merge([''], $languages);
-    echo $form->field($userLanguage, 'language_id')->dropDownList($nativeLangList, ['name' => 'languages[1]', 'id' => 'native-lang', 'class' => 'lang-field'])->label('Native language'); 
+    echo $form->field($userLanguage, 'language_id')->dropDownList($nativeLangList, ['name' => 'languages[0]', 'id' => 'native-lang', 'class' => 'lang-field'])->label('Native language'); 
 ?>
 <div class="optional-languages">
 <?php
     $parents = ['native-lang'];
     $optLanguages = ContactsHelper::getOptLanguages(Yii::$app->user->id);
-    for($key = 2; $key <= count($languages); $key++){
+    for($key = 1; $key < count($languages); $key++){
         echo $form->field($userLanguage, 'language_id')->widget(DepDrop::classname(), [
             'data' => !empty($optLanguages) ? array_shift($optLanguages) : [],
             'options' => ['id' => "option-lang-$key", 'name' => "languages[$key]", 'class' => 'lang-field'],
