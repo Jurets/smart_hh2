b740025d8f4822f9df79fb038ab70bc7e9fc0def
andrew <volpacchiotto@ukr.net>
2015-04-30 23:24:30 +0300

    опять пропатчены окна в кабинете возле языков

--- frontend/views/registration/_languages_form.php
+++ frontend/views/registration/_languages_form.php
@@ -5,6 +5,14 @@ use frontend\helpers\ContactsHelper;
 
 <?php
     $nativeLangList = !empty($userLanguage->language_id) ? $languages : array_merge([''], $languages);
+    
+    if(isset($nativeLangList[0])){
+        $nativeLangList = $languages =  \common\components\Commonhelper::LaPatch();
+    }
+    if(empty($userLanguage)){
+        $userLanguage = \common\components\Commonhelper::LaPatch2();
+    }
+        
     echo $form->field($userLanguage, 'language_id')->dropDownList($nativeLangList, ['name' => 'languages[0]', 'id' => 'native-lang', 'class' => 'lang-field'])->label('Native language'); 
 ?>
 <div class="optional-languages">
